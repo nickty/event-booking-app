1f0aa3d81798dd47c067dd7717293d4db1c52695
Mizanur Rahman <nickty.86@gmail.com>
2021-03-18 16:53:31 +0600

    first mutation dummy

--- app.js
+++ app.js
@@ -6,17 +6,34 @@ const { buildSchema, } = require('graphql')
 
 const app = express()
 
+const events = []
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: String!
+        date: String!
+    }
     
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema {
             query: RootQuery
@@ -25,11 +42,19 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic', 'Sailing']
+           return events
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+
+            events.push(event)
+            return event
         }
     },
     graphiql: true
